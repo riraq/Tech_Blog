73d01c2da55b2bd072acfa24fd830a08b9cbc3ed
riraq <riraq@mail.usf.edu>
2021-04-08 19:10:27 -0500

    added seed files, linked handlebar view to homepage route

--- models/Post.js
+++ models/Post.js
@@ -20,7 +20,7 @@ Post.init(
             allowNull: false,
         },
         date_created: {
-            type: DataTypes.DATE,
+            type: DataTypes.STRING,
             allowNull: false,
             defaultValue: DataTypes.NOW,
         },
