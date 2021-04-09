ae2e98438bf6470c58205e12413a0ec8ddbe9fc4
riraq <riraq@mail.usf.edu>
2021-04-09 18:25:55 -0500

    updated route for user dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -52,25 +52,25 @@ router.get('/post/:id', async (req, res) => {
     }
 });
 
-// // Use withAuth middleware to prevent access to route
-// router.get('/dashboard', withAuth, async (req, res) => {
-//     try {
-//         // Find the logged in user based on the session ID
-//         const userData = await User.findByPk(req.session.user_id, {
-//             attributes: { exclude: ['password'] },
-//             include: [{ model: Post }],
-//         });
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
 
-//         const user = userData.get({ plain: true });
-//         console.log(user);
-//         res.render('dashboard', {
-//             user,
-//             logged_in: true
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+        const user = userData.get({ plain: true });
+        console.log(user);
+        res.render('dashboard', {
+            user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect to their dashboard
