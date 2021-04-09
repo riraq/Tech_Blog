29811d2e6ebb67bf51f0c1beda53369c428ce1f5
riraq <riraq@mail.usf.edu>
2021-04-09 18:20:14 -0500

    updated route for login/signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
@@ -53,18 +53,18 @@ router.get('/post/:id', async (req, res) => {
 });
 
 // // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
+// router.get('/dashboard', withAuth, async (req, res) => {
 //     try {
 //         // Find the logged in user based on the session ID
 //         const userData = await User.findByPk(req.session.user_id, {
 //             attributes: { exclude: ['password'] },
-//             include: [{ model: Project }],
+//             include: [{ model: Post }],
 //         });
 
 //         const user = userData.get({ plain: true });
-
-//         res.render('profile', {
-//             ...user,
+//         console.log(user);
+//         res.render('dashboard', {
+//             user,
 //             logged_in: true
 //         });
 //     } catch (err) {
@@ -72,14 +72,13 @@ router.get('/post/:id', async (req, res) => {
 //     }
 // });
 
-// router.get('/login', (req, res) => {
-//     // If the user is already logged in, redirect the request to another route
-//     if (req.session.logged_in) {
-//         res.redirect('/profile');
-//         return;
-//     }
-
-//     res.render('login');
-// });
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect to their dashboard
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('loginpage');
+});
 
 module.exports = router;
