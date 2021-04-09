5c90f53d43fbb13c47a9f2cc4c332f952faae6ad
riraq <riraq@mail.usf.edu>
2021-04-09 16:20:04 -0500

    updated route to show data for single post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, /*Comment*/ } = require('../models');
+const { User, Post, Comment } = require('../models');
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -17,7 +17,6 @@ router.get('/', async (req, res) => {
         // Serialize data so the template can read it
         const posts = postData.map((post) => post.get({ plain: true }));
 
-        console.log('postData: ', posts);
         // Pass serialized data and session flag into template
         res.render('homepage', {
             posts,
@@ -28,27 +27,30 @@ router.get('/', async (req, res) => {
     }
 });
 
-// router.get('/post/:id', async (req, res) => {
-//     try {
-//         const projectData = await Project.findByPk(req.params.id, {
-//             include: [
-//                 {
-//                     model: User,
-//                     attributes: ['name'],
-//                 },
-//             ],
-//         });
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment
+                }
+            ],
+        });
 
-//         const project = projectData.get({ plain: true });
+        const posts = postData.get({ plain: true });
 
-//         res.render('project', {
-//             ...project,
-//             logged_in: req.session.logged_in
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+        res.render('postpage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // // Use withAuth middleware to prevent access to route
 // router.get('/profile', withAuth, async (req, res) => {
